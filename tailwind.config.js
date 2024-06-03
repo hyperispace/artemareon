57ccd8152be67b28a86d5cb53b958cb820d28bf8
Hassen Ahmed <69512496+hassanidris@users.noreply.github.com>
2024-06-03 14:58:02 +0200

    Ha/adjusting fonts (#21)
    * introducing two fonts
    
    * adding rocket img

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,77 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        openSans: ['Open Sans', 'sans-serif'],
+      },
+      fontSize: {
+        '5xl': [
+          '3rem',
+          {
+            lineHeight: '2em',
+            fontWeight: '700',
+          },
+        ],
+        '4xl': [
+          '2rem',
+          {
+            lineHeight: '2em',
+            fontWeight: '700',
+          },
+        ],
+        '3xl': [
+          '1.75rem',
+          {
+            lineHeight: '2em',
+            fontWeight: '700',
+          },
+        ],
+        '2xl': [
+          '1.5rem',
+          {
+            lineHeight: '1.75em',
+            fontWeight: '700',
+          },
+        ],
+        xl: [
+          '1.35rem',
+          {
+            lineHeight: '1.75em',
+            fontWeight: '600',
+          },
+        ],
+        lg: [
+          '1.15rem',
+          {
+            lineHeight: '1.75em',
+            fontWeight: '600',
+          },
+        ],
+        base: [
+          '1.1rem',
+          {
+            lineHeight: '1.5em',
+            fontWeight: '400',
+          },
+        ],
+        sm: [
+          '.9rem',
+          {
+            lineHeight: '1.25em',
+            fontWeight: '400',
+          },
+        ],
+        xs: [
+          '.7rem',
+          {
+            lineHeight: '.8em',
+            fontWeight: '400',
+          },
+        ],
+      },
+    },
   },
   plugins: [],
 };
