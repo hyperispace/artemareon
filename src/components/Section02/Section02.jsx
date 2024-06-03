9024f7e1cae47fa519433098f5fbfed2c3980040
Niklas Wallenborg <niklas.wallenborg@hyperisland.se>
2024-06-03 12:30:18 +0200

    add subtitle to section 2 and 10 (#19)

--- src/components/Section02/Section02.jsx
+++ src/components/Section02/Section02.jsx
@@ -2,11 +2,13 @@ import Heading from '../common/Heading';
 import LaikaImg from './LaikaImg';
 import './Section02.css';
 import StarWarsText from './StarWarsText';
+import SubHeadings from '../common/SubHeadings';
 
 const Section02 = () => {
   return (
     <section className='section02-background'>
       <Heading title='The First Living Being in Space' />
+      <SubHeadings title='Sputnik 2' />
       <LaikaImg />
       <StarWarsText />
     </section>
