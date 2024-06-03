9af4bffd6c373bdba9ca4a6868d11c03bbf8b163
Jinjing Wu <jinjing.wu@compass.com>
2024-06-03 16:01:02 +0200

    feat: gateway image

---
+++ src/components/Section9/Gateway.jsx
@@ -0,0 +1,35 @@
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { useRef } from 'react';
+
+/* eslint-disable react/prop-types */
+const Gateway = ({ src, width, height }) => {
+  const imageRef = useRef(null);
+  useGSAP(() => {
+    if (!imageRef.current) return;
+    gsap
+      .timeline({
+        x: '10%',
+        y: '0%',
+        opacity: 0.7,
+        scrollTrigger: {
+          trigger: imageRef.current,
+          start: 'top 80%',
+          end: 'bottom 20%',
+          scrub: true,
+        },
+      })
+      .to(imageRef.current, {
+        x: '100%',
+        y: '10%',
+        scale: 2,
+        transformOrigin: 'center',
+        opacity: 1,
+        duration: 3,
+        ease: 'power1.inOut',
+      });
+  }, []);
+  return <img ref={imageRef} src={src} width={width} height={height} />;
+};
+
+export default Gateway;
--- src/components/Section9/Section9.jsx
+++ src/components/Section9/Section9.jsx
@@ -2,18 +2,21 @@ import Heading from '../common/Heading';
 import SubHeadings from '../common/SubHeadings';
 import PopUpText from '../common/PopUpText';
 import InfoBox from '../common/InfoBox';
+import Gateway from './Gateway';
 
 const Section9 = () => {
   return (
     <>
-      <section className='panel relative w-full h-full flex flex-col justify-start items-start pt-5'>
-        <Heading title='Lunar Gateway' />
-        <SubHeadings title='Building the Infrastructure for Interplanetary Travel' />
+      <section className='panel relative w-full h-full flex flex-col pt-5 items-start justify-between'>
+        <div>
+          <Heading title='Lunar Gateway' />
+          <SubHeadings title='Building the Infrastructure for Interplanetary Travel' />
+        </div>
         <InfoBox
           title='Lunar Gateway'
           text='Is a planned space station that will orbit the Moon, serving as a staging point for lunar exploration and as a gateway to deep space destinations like Mars. It will be assembled in lunar orbit over several missions.Crewed missions to Lunar Gateway will enable astronauts to conduct lunar surface expeditions, test technologies for future Mars missions, and explore the Moon&#39;s unique environment, including its resources and potential for sustained habitation'
         />
-
+        <Gateway width='200px' height='200px' src='./assets/lunargateway.png' />
         <PopUpText text='Towards a Multi-Planetary Future' />
       </section>
     </>
