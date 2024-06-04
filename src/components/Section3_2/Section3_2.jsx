c5214d6104672665105b6b49d4272b99ca5d8137
Jinjing Wu <wujinjing25@gmail.com>
2024-06-04 20:00:19 +0200

    fix: section 3 (#42)

--- src/components/Section3_2/Section3_2.jsx
+++ src/components/Section3_2/Section3_2.jsx
@@ -5,12 +5,11 @@ import CommonImage from '../common/CommonImage';
 const Section3_2 = () => {
   return (
     <>
-      <div className='panel relative w-full h-full flex flex-col items-start justify-evenly pt-5'>
-        <div>
-          <SubHeadings title='Valentina Tereshkova: The First Woman in Space' />
-        </div>
-        <div className=' w-full h-full flex justify-end items-start absolute right-[20vw] top-[50vh]'>
-          <div className=' w-[30vw]'>
+      {/* <div className='panel relative w-full h-full flex flex-col items-start justify-evenly pt-5'> */}
+      <div className='panel relative w-full h-full pt-5 grid'>
+        <SubHeadings title='Valentina Tereshkova: The First Woman in Space' />
+        <div className='w-[30vw] h-full flex justify-start items-start absolute left-[8vw] top-[20vh]'>
+          <div className='w-[30vw]'>
             <InfoBox
               title='Valentina Tereshkova'
               text='On June 16, 1963, Valentina Tereshkova, a Soviet cosmonaut, became the first woman to travel into space. Her mission aboard Vostok 6 was a significant achievement in the history of space exploration and women&#39;s role in science and technology. Vostok 6 launched from Baikonur Cosmodrome, It carried Tereshkova into orbit for a solo flight lasting nearly three days and orbited Earth 48 times.Tereshkova&#39;s call sign was "Chaika" (Seagull), and her flight was a major milestone for women in space.'
@@ -21,8 +20,9 @@ const Section3_2 = () => {
         <CommonImage
           width='180px'
           height='240px'
-          classes='rounded-lg'
+          classes='rounded-lg place-self-end'
           src='./assets/Valentina.jpg'
+          direction='right'
         />
       </div>
     </>
