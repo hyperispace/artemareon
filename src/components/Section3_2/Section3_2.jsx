3696988fbf0579b444823da52a845c829f0ff241
Hassen Ahmed <hassan.creative@gmail.com>
2024-06-04 17:42:26 +0200

    fixing jinjing branch

--- src/components/Section3_2/Section3_2.jsx
+++ src/components/Section3_2/Section3_2.jsx
@@ -9,10 +9,14 @@ const Section3_2 = () => {
         <div>
           <SubHeadings title='Valentina Tereshkova: The First Woman in Space' />
         </div>
-        <InfoBox
-          title='Valentina Tereshkova'
-          text='On June 16, 1963, Valentina Tereshkova, a Soviet cosmonaut, became the first woman to travel into space. Her mission aboard Vostok 6 was a significant achievement in the history of space exploration and women&#39;s role in science and technology. Vostok 6 launched from Baikonur Cosmodrome, It carried Tereshkova into orbit for a solo flight lasting nearly three days and orbited Earth 48 times.Tereshkova&#39;s call sign was "Chaika" (Seagull), and her flight was a major milestone for women in space.'
-        />
+        <div className=' w-full h-full flex justify-end items-start absolute right-[20vw] top-[50vh]'>
+          <div className=' w-[30vw]'>
+            <InfoBox
+              title='Valentina Tereshkova'
+              text='On June 16, 1963, Valentina Tereshkova, a Soviet cosmonaut, became the first woman to travel into space. Her mission aboard Vostok 6 was a significant achievement in the history of space exploration and women&#39;s role in science and technology. Vostok 6 launched from Baikonur Cosmodrome, It carried Tereshkova into orbit for a solo flight lasting nearly three days and orbited Earth 48 times.Tereshkova&#39;s call sign was "Chaika" (Seagull), and her flight was a major milestone for women in space.'
+            />
+          </div>
+        </div>
 
         <CommonImage
           width='180px'
