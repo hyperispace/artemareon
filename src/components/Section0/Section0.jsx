ae3c7dbcc6e38c9561f9208d41dcf801839b5f6c
Hassen Ahmed <69512496+hassanidris@users.noreply.github.com>
2024-06-04 15:05:34 +0200

    adjusting  rocket starting (#34)

--- src/components/Section0/Section0.jsx
+++ src/components/Section0/Section0.jsx
@@ -1,16 +1,39 @@
+import { useGSAP } from '@gsap/react';
 import { rocketIcon } from '../../constant/images';
 import Heading from '../common/Heading';
 import SubHeadings from '../common/SubHeadings';
+import { useRef } from 'react';
+import gsap from 'gsap';
 
 const Section0 = () => {
+  const rocketIconRef = useRef(null);
+  const startTimeline = useRef(gsap.timeline());
+
+  useGSAP(() => {
+    startTimeline.current
+      .from(
+        rocketIconRef.current,
+        { opacity: 0, scale: 2, y: -50, duration: 2 },
+        '+=1',
+      )
+      .to(rocketIconRef.current, {
+        y: 300,
+        duration: 1,
+        scale: 0.3,
+      })
+      .to(rocketIconRef.current, { y: 250, repeat: -1, duration: 1 });
+  });
   return (
     <section className='section02-background'>
       <div className=' flex flex-col justify-center'>
         <Heading title='Scroll Through Space History' />
         <SubHeadings title='From Early Days to Visionary Plans' />
       </div>
-      <div className=' absolute flex justify-center items-center h-full w-full z-40'>
-        <img className=' text-white' src={rocketIcon} alt='' />
+      <div
+        ref={rocketIconRef}
+        className=' absolute flex justify-center items-center h-full w-full z-40'
+      >
+        <img className=' w-64' src={rocketIcon} alt='' />
       </div>
     </section>
   );
