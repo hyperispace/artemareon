39e05fa611b080beb461585c31fa22841cfa8aba
Hassen Ahmed <hassan.creative@gmail.com>
2024-06-05 11:05:37 +0200

    fixing font

--- src/components/Sputnik/Sputnik.jsx
+++ src/components/Sputnik/Sputnik.jsx
@@ -11,14 +11,14 @@ const Sputnik = () => {
       <Heading title='The Dawn of the Space Age' />
       <SubHeadings title='Sputnik 1' />
       {/* <div className='block'></div> */}
-      <div className=' absolute h-full w-[40vw] flex justify-start items-center ml-20 top-[10px] z-20'>
+      <div className=' absolute h-full w-[30vw] flex justify-start items-center ml-20'>
         <InfoBox
           title="Humanity's First Artificial Satellite"
           text="On October 4, 1957, the Soviet Union launched Sputnik 1, the world's first artificial satellite. This groundbreaking event marked the beginning of the space age and the start of the space race between the United States and the Soviet Union. Sputnik 1 was a polished metal sphere, 23 inches in diameter, with four external radio antennas. It transmitted radio pulses back to Earth Sputnik 1 orbited Earth every 96 minutes, traveling at a speed of about 18,000 miles per hour."
         />
       </div>
       <SputnikImage />
-      {/* <PopUpText text='Its beeps echoed worldwide' /> */}
+      <PopUpText text='Its beeps echoed worldwide' />
     </div>
   );
 };
